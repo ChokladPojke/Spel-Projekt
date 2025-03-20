309faf19410364d09e560d4b569e36c688a51077
ChokladPojke <nicpalm06@gmail.com>
2025-03-20 13:38:14 +0100

    Major Update

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal;
+    public float speed = 8f;
+    public float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    private bool canDash = true;
+    private bool isDashing;
+    private bool hasDashed = false;
+    public float dashingPower = 24f;
+    public float dashingTime = 0.2f;
+    public float dashingCooldown = 1f;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private BoxCollider2D groundCheckCollider; // Use BoxCollider2D instead of groundCheck
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private BoxCollider2D wallCheckCollider; // Use BoxCollider2D instead of wallCheck
+    [SerializeField] private TrailRenderer tr;
+
+    private void Update()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonDown("Jump") && CanWallJump())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash && !hasDashed)
+        {
+            StartCoroutine(Dash());
+        }
+
+        if (IsGrounded())
+        {
+            hasDashed = false; 
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool CanWallJump()
+    {
+        return wallCheckCollider.IsTouchingLayers(groundLayer) && !IsGrounded();
+    }
+
+    private bool IsGrounded()
+    {
+        return groundCheckCollider.IsTouchingLayers(groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            Vector3 localScale = transform.localScale;
+            isFacingRight = !isFacingRight;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        hasDashed = true;
+
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+
+        yield return new WaitForSeconds(dashingTime);
+        
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
+}
